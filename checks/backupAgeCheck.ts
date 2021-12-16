3b6d1e42bb1d30ab182e7466b375ad7e476012aa
Jake Saint Germain <jakesaint27@gmail.com>
2021-12-15 23:48:32 -0500

    bug fix for deleting old backups

--- checks/backupAgeCheck.ts
+++ checks/backupAgeCheck.ts
@@ -1,8 +1,36 @@
 const findRemoveSync = require('find-remove');
 
-export const checkForOldBackups = (deleteUnitOfTime: string, deleteInterval: number) => {
-    findRemoveSync('../WorldBackups', {
-        age: {[`${deleteUnitOfTime}`]: deleteInterval},
+let deleteInterval: number = 0;
+
+let allowedIncrementsOftime: string[] = [ //TOD: make this more useful...
+    'seconds',
+    'minutes',
+    'hours',
+    'days'
+];
+
+const checkForAllowedTimeIntervals = (deleteUnitOfTime: string, possibleDeleteInterval: number) => {
+    if (deleteUnitOfTime === 'seconds') {
+        deleteInterval = possibleDeleteInterval;
+    } else if (deleteUnitOfTime === 'minutes') {
+        deleteInterval = possibleDeleteInterval*60;
+    } else if (deleteUnitOfTime === 'hours') {
+        deleteInterval = possibleDeleteInterval*3600;
+    } else if (deleteUnitOfTime === 'days') {
+        deleteInterval = possibleDeleteInterval*24*3600;
+    } else {
+        console.log(`Invalid increment of time for file deletion.`);
+        console.log(`Please enter ${allowedIncrementsOftime}`);
+        process.exit();
+    }
+    return deleteInterval;
+}
+
+export const checkForOldBackups = (deleteUnitOfTime: string, possibleDeleteInterval: number) => {
+    let returnedInterval = checkForAllowedTimeIntervals(deleteUnitOfTime, possibleDeleteInterval);
+
+    findRemoveSync('../WorldBackups', {    
+        age: {seconds: returnedInterval},
         extensions: '.zip'},
     );
 }
